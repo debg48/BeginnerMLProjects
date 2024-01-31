8a6c0724aaf27f5b3689cd4fed6a037bee4aa1f4
Archan Ghosh <gharchan@gmail.com>
2024-01-31 17:04:28 +0000

    added class transformer decoder

--- Deep Learning/base_transformer.py
+++ Deep Learning/base_transformer.py
@@ -238,3 +238,48 @@ class DecoderBlock(nn.Module):
         out = self.transformer_block(key, query, value)
 
         return out 
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, target_vocab_size, embed_dim, seq_len, num_layers = 2, expansion_factor = 4, n_heads = 8):
+        super(TransformerDecoder, self).__init__()
+        '''
+        Arguments:
+                 target_vocab_size:
+                 embed_dim:
+                 seq_len:
+                 num_layers:
+                 expansion_factor:
+                 n_heads:  
+        '''
+        self.word_embedding = nn.Embedding(target_vocab_size, embed_dim)
+        self.position_embedding = PositionalEmbedding(seq_len, embed_dim)
+
+        self.layers = nn.ModuleList(
+                    [
+                        DecoderBlock(embed_dim, expansion_factor = 4, n_heads = 8)
+                        for _ in range(num_layers)
+                    ]
+        )
+        self.fc_out = nn.Linear(embed_dim, target_vocab_size)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x, enc_out, mask):
+        '''
+        Arguments:
+                 x:
+                 enc_out:
+                 trg_mask:
+        Returns:
+                 out:  
+        '''
+
+        x = self.word_embedding(x)
+        x = self.position_embedding(x)
+        x = self.dropout(x)
+
+        for layer in self.layers:
+            x = layer(enc_out, x, enc_out, mask)
+        
+        out = F.softmax(self.fc_out(x))
+
+        return out 
