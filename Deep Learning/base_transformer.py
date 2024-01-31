e32d306adfec8223b78b232bacb05ecd3d8753b5
Archan Ghosh <gharchan@gmail.com>
2024-01-31 16:43:55 +0000

    added transformer encoder class

--- Deep Learning/base_transformer.py
+++ Deep Learning/base_transformer.py
@@ -177,3 +177,34 @@ class TransfomerBlock(nn.Module):
 
         return norm2_out 
 
+class TransformerEncoder(nn.Module):
+    '''
+    Arguments:
+             seq_len:
+             embed_dim:
+             num_layers:
+             expansion_factors:
+             n_heads:
+
+    Returns:
+             out:
+    '''
+
+    def __init__(self, seq_len, vocab_size, embed_dim, num_layers = 2, expansion_factor = 4, n_heads = 8):
+        super(TransformerEncoder, self).__init__()
+
+        self.embedding_layer = Embedding(vocab_size, embed_dim)
+
+        self.positional_encoder = PositionalEmbedding(seq_len, embed_dim)
+
+        self.layers = nn.ModuleList([TransformerBlock(embed_dim, expansion_factor, n_heads) for i in range (num_layers)])
+
+    def forward(self, x):
+        embed_out = self.embedding_layer(x)
+
+        out = self.positional_encoder(embed_out)
+
+        for layer in self.layers:
+            out = layer(out, out, out)
+        
+        return out 
