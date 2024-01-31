2fa8db759a7fceb2603eb698db037f66a9099de0
Archan Ghosh <gharchan@gmail.com>
2024-01-31 17:32:13 +0000

    added class transformer

--- Deep Learning/base_transformer.py
+++ Deep Learning/base_transformer.py
@@ -283,3 +283,64 @@ class TransformerDecoder(nn.Module):
         out = F.softmax(self.fc_out(x))
 
         return out 
+
+class Transformer(nn.Module):
+    def __init__(self, embed_dim, src_vocab_size, target_vocab_size, seq_length,num_layers=2, expansion_factor=4, n_heads=8):
+        super(Transformer, self).__init__()
+        
+        """  
+        Arguments:
+        
+        """
+        
+        self.target_vocab_size = target_vocab_size
+
+        self.encoder = TransformerEncoder(seq_length, src_vocab_size, embed_dim, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads)
+        self.decoder = TransformerDecoder(target_vocab_size, embed_dim, seq_length, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads)
+        
+    
+    def make_trg_mask(self, trg):
+        """
+        Arguments:
+        """
+        batch_size, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+            batch_size, 1, trg_len, trg_len
+        )
+        return trg_mask    
+
+    def decode(self,src,trg):
+        """
+        Arguments:
+
+        Returns:
+        """
+        trg_mask = self.make_trg_mask(trg)
+        enc_out = self.encoder(src)
+        out_labels = []
+        batch_size,seq_len = src.shape[0],src.shape[1]
+        out = trg
+        for i in range(seq_len): 
+            out = self.decoder(out,enc_out,trg_mask) 
+
+            out = out[:,-1,:]
+     
+            out = out.argmax(-1)
+            out_labels.append(out.item())
+            out = torch.unsqueeze(out,axis=0)
+          
+        
+        return out_labels
+    
+    def forward(self, src, trg):
+        """
+        Argsuments:
+
+        Returns:
+
+        """
+        trg_mask = self.make_trg_mask(trg)
+        enc_out = self.encoder(src)
+   
+        outputs = self.decoder(trg, enc_out, trg_mask)
+        return outputs
